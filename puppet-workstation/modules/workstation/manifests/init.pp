bc9f1d54780cf80f77e8f35b4ace3c913e4f1634
Dylan Cochran <heliocentric@gmail.com>
2016-11-22 12:58:13 -0500

    Add workstation

---
+++ puppet-workstation/modules/workstation/manifests/init.pp
@@ -0,0 +1,87 @@
+class workstation() {
+	case downcase($osfamily) {
+		"windows" : {
+			$packages = [
+				"sourcetree",
+				"vagrant",
+				"virtualbox",
+				"mobaxterm",
+				"notepadplusplus.install",
+				"git",
+				"7zip",
+				"netbeans-jee",
+				"sysinternals",
+				"powershell",
+				"filezilla",
+				"ruby",
+				"ruby2.devkit",
+				"keepass",
+				"syncthing",
+				"syncthing-gtk",
+				"putty",
+				"slack",
+				"vmwarevsphereclient",
+			]
+			include ::chocolatey
+			$package_provider = "chocolatey"
+		}
+		"freebsd" : {
+			$packages = [
+				"dwm",
+				"chromium",
+				"firefox",
+				"netbeans",
+				"inkscape",
+				"gimp",
+				"blender",
+				"quassel",
+				"nmap",
+				"bash",
+				"krdc",
+				"mono",
+				"libgdiplus",
+				"keepass",
+				"openvpn",
+				"gitflow",
+				"sudo",
+				"shutter",
+				"virtualbox-ose",
+				"vagrant",
+				"docker-freebsd",
+				"synergy",
+				"syncthing",
+				"iocage",
+				"x11vnc",
+				"dmenu",
+				"lxterminal",
+				"zxfer",
+				"slock",
+			]
+			$package_provider = "pkgng"
+			file { "/usr/local/bin/vnc":
+				source => "puppet:///modules/workstation/vnc",
+				mode => "755",
+				owner => "root",
+			}
+			file { "/usr/local/bin/novnc":
+				source => "puppet:///modules/workstation/novnc",
+				mode => "755",
+				owner => "root",
+			}
+			file { "/usr/local/bin/dwm-syn":
+				source => "puppet:///modules/workstation/dwm-syn",
+				mode => "755",
+				owner => "root",
+			}
+			file { "/usr/local/share/xsessions/49dwm.desktop":
+				source => "puppet:///modules/workstation/49dwm.desktop",
+				mode => "755",
+				owner => "root",
+			}
+		}
+	}
+	package { $packages:
+		ensure => "installed",
+		provider => $package_provider,
+	}
+}
