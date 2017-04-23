e05d211a96204c933942d89138e51eae2672cf55
Dylan Cochran <heliocentric@gmail.com>
2017-04-22 21:05:19 -0400

    Add libXScreenSaver

--- puppet-workstation/modules/workstation/manifests/init.pp
+++ puppet-workstation/modules/workstation/manifests/init.pp
@@ -97,6 +97,7 @@ class workstation() {
 				"vim-enhanced",
 			]
 			package { 'lsb': }
+			package { 'libXScreenSaver': }
 			package { "google-chrome":
 				source   => 'https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm',
 				provider => 'rpm',
